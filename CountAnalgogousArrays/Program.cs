8d78ff349c743570a13d0aa3e6303be3711b7600
ZKrakus <ZKrakus@gmail.com>
2021-07-26 16:12:14 -0400

    Added two new Amazon interview Questions
    Changed project names to better reflect project content.

---
+++ CountAnalgogousArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AmazonQuestion2
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            countAnalogousArrays(new List<int> { -2,-1,0,1,2}, -2, 12);
+        }
+
+        public static int countAnalogousArrays(List<int> consecutiveDifference, int lowerBound, int upperBound) {
+            var length = consecutiveDifference.Count;
+            var analogousArr = new int[length];
+
+            int accumulator = lowerBound;
+            int localUpperBound = int.MinValue;
+            int localLowerBound = int.MaxValue;
+            for (int i = 0; i < length; i++) {
+                analogousArr[i] = accumulator + consecutiveDifference[i];
+                localUpperBound = Math.Max(consecutiveDifference[i], localUpperBound);
+                localLowerBound = Math.Min(consecutiveDifference[i], localLowerBound);
+            }
+
+            int analogs = 0, lower = localLowerBound, upper = localUpperBound;
+            for(int i = localLowerBound; i <= upperBound; i++) {
+                if (lower >= lowerBound && upper <= upperBound)
+                    analogs++;
+                lower++; upper++;
+            }
+            
+            return analogs;
+        }
+    }
+}
---
+++ GroupTransactions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AmazonQuestion1
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            groupTransactions(new List<string> { "notebook", "notebook", "mouse", "keyboard", "mouse" });
+        }
+
+        // notebook notebook mouse keyboard mouse
+        public static List<string> groupTransactions(List<string> transactions) {
+            if (transactions == null)
+                return new List<string>();
+
+            Dictionary<string, int> transDict = new Dictionary<string, int>();
+            foreach (string trans in transactions) {
+                if (transDict.TryGetValue(trans, out int count)) {
+                    transDict.Remove(trans);
+                    transDict.Add(trans, ++count);
+                } else {
+                    transDict.Add(trans, 1);
+                }
+            }
+
+            int i = 0;
+            KeyValuePair<string, int>[] keyValArr = new KeyValuePair<string, int>[transDict.Count];
+            foreach (KeyValuePair<string, int> keyVal in transDict) {
+                keyValArr[i] = keyVal;
+                i++;
+            }
+
+            i = 0;
+            var orderedTransacitons = keyValArr.OrderByDescending(x => x.Value).ThenBy(y => y.Key);
+            List<string> summedTransactions = new List<string>();
+            foreach (var item in orderedTransacitons) {
+                summedTransactions.Add(item.Key.ToString() + ' ' + item.Value.ToString());
+                i++;
+            }
+
+            return summedTransactions;
+        }
+    }
+}
